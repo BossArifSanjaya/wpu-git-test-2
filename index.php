20a4e042eb091620a362e3553609b5538f140498
BossArifSanjaya <bossarif777@gmail.com>
2023-11-15 14:29:07 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dicta eius ratione accusantium amet animi suscipit dolorem consequatur voluptatum culpa fuga delectus nemo, eum ipsa sunt explicabo recusandae accusamus itaque.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
